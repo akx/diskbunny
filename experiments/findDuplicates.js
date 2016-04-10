dc790cc68e80586cb8efe5eee1192ecdd4d599fc
Aarni Koskela <akx@iki.fi>
2016-04-10 22:54:58 +0300

    Add a duplicate finder

--- experiments/findDuplicates.js
+++ experiments/findDuplicates.js
@@ -1,7 +1,36 @@
 /* eslint-disable no-console */
 import Scanner from '../lib/scanner';
 import {extname} from 'path';
+import {printProgress} from './utils';
+import R from 'ramda';
+import Promise from 'bluebird';
+import fs from 'fs';
+import {createHash} from 'crypto';
+const openAsync = Promise.promisify(fs.open);
+const readAsync = Promise.promisify(fs.read, {multiArgs: true});
 
+const hashBytes = 524288;
+
+function hashFile(file) {
+  let fileFd = null;
+  return openAsync(file, 'r').then((fd) => {
+    const buf = new Buffer(hashBytes);
+    fileFd = fd; // For closing.
+    return readAsync(fd, buf, 0, hashBytes, null);
+  }).then(([bytesRead, buf]) => {
+    fs.close(fileFd);
+    const hash = createHash('sha1');
+    hash.update(buf.slice(0, bytesRead));
+    return hash.digest('hex');
+  });
+}
+
+function hashGroup(group) {
+  const filenames = R.pluck('path', group);
+  return Promise.map(filenames, hashFile, {concurrency: 5}).then((hashes) => {
+    return (R.all(R.equals(hashes[0]), hashes) ? {hash: hashes[0], group} : null);
+  });
+}
 
 export default function findDuplicates(roots) {
   const scanner = new Scanner({roots});
@@ -10,11 +39,25 @@ export default function findDuplicates(roots) {
     clearInterval(progressTimer);
     const groups = {};
     scanner.getAllDirectories().forEach((dir) => {
-      dir.entries.forEach((ent) => {
-        const groupKey = `${extname(ent.path)} ${ent.size}`;
+      dir.entries.filter((ent) => ent.isFile()).forEach((ent) => {
+        const groupKey = `${extname(ent.path).toLowerCase()}\n${ent.size}`;
         (groups[groupKey] = groups[groupKey] || []).push(ent);
       });
     });
-    console.log(`${Object.keys(groups).length} groups.`);
+    const viableGroups = R.pipe(
+      R.values,
+      R.filter((g) => g.length > 1)
+    )(groups);
+    console.log(`${viableGroups.length} groups with possible duplicates.`);
+    return Promise.map(viableGroups, hashGroup, {concurrency: 5}).then((hashedGroups) => {
+      return hashedGroups.filter(R.identity);
+    });
+  }).then((hashedGroups) => {
+    hashedGroups.forEach(({hash, group}) => {
+      console.log(`\n### ${hash}`);
+      group.forEach((stat) => {
+        console.log(`+ ${stat.path}`);
+      });
+    });
   });
 }
